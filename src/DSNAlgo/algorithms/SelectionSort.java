071218421799de39a8f212943c6f7ed223ad4f0e
Shareef112 <31471800+Shareef112@users.noreply.github.com>
2025-09-02 11:02:41 +0530

    Update SelectionSort.java

--- src/DSNAlgo/algorithms/SelectionSort.java
+++ src/DSNAlgo/algorithms/SelectionSort.java
@@ -10,17 +10,14 @@ public class SelectionSort {
     }
 
     public static void sort(int[] arr){
-        int n = arr.length;
-        for(int i=0;i<n-1;i++){
-            int min = i;
-            for(int j=i+1;j<n;j++){
-                if(arr[min] > arr[j]){
-                    min = j;
-                }
-            }
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
+       int n = arr.length;
+    for(int i=1;i<n;i++){ //unsorted portion
+      int temp = arr[i];
+      int j = i-1; // sorted portion
+      while (j>=0 && arr[j] > temp){
+        arr[j+1] = arr[j];
+        j--;
+      }
+      arr[j+1] = temp;
     }
 }
